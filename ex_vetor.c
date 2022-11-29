bc6e71d7644616471a7ce2e542d9b1d3476fd939
Renan-Pecanha <112291246+Renan-Pecanha@users.noreply.github.com>
2022-11-29 09:07:16 -0300

    Add files via upload

---
+++ ex_vetor.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define TAM 5	
+	
+	int main ()
+{ 
+	int vet [TAM], i, menor, soma=0;
+	
+	for(i=0; i < TAM ; i++)
+	{
+		printf("Digite um valor: ");
+		scanf("%i",&vet[i]);
+	}
+	menor =vet[0];
+	{ 
+		vet[i]=i;	
+	}
+	
+	
+	
+	for(i=0; i < TAM ; i++) 
+	
+	
+	
+
+		if (menor > vet[i])
+		{
+			menor = vet[i] ;
+		}
+		printf("\nO menor valor = %.2i", menor);
+		for (i=0; i < TAM; i++)
+		{
+			if (menor > vet[i]);
+			soma =vet[1];
+		}
+		
+		
+		printf("%i", soma);
+		printf("\n%i", menor);	
+
+			
+	
+	
+	return 0;
+}	
